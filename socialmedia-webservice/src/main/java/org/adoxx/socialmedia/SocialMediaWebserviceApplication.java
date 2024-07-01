b54c92548b09d1390fbea149bbca9b6f400b6774
Victor Binzar <a12022144@unet.univie.ac.at>
2024-07-01 21:30:20 +0200

    Project setup take 2

---
+++ socialmedia-webservice/src/main/java/org/adoxx/socialmedia/SocialMediaWebserviceApplication.java
@@ -0,0 +1,13 @@
+package org.adoxx.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaWebserviceApplication.class, args);
+	}
+
+}
---
+++ socialmedia-webservice/src/test/java/org/adoxx/socialmedia/SocialMediaWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adoxx.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
