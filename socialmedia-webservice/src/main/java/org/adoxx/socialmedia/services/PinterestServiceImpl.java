e3d2a669389db24437b75617a90e2467984ed07f
Victor Binzar <a12022144@unet.univie.ac.at>
2024-07-08 21:19:18 +0300

    Closes #1

--- socialmedia-webservice/src/main/java/org/adoxx/socialmedia/services/PinterestServiceImpl.java
+++ socialmedia-webservice/src/main/java/org/adoxx/socialmedia/services/PinterestServiceImpl.java
@@ -3,9 +3,7 @@ package org.adoxx.socialmedia.services;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.adoxx.socialmedia.exceptions.BoardException;
-import org.adoxx.socialmedia.exceptions.InitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -22,16 +20,6 @@ public class PinterestServiceImpl implements IPinterestService {
         this.webClient = webClient;
     }
 
-/*    public ResponseEntity<String> init(String accessToken) {
-        try {
-            this.pinterestClient = new Pinterest(accessToken);
-            log.info("Pinterest client initialized successfully.");
-            return ResponseEntity.ok("Pinterest client initialized successfully.");
-        } catch (Exception e) {
-            log.error("Error initializing Pinterest client: ", e);
-            throw new InitializationException("Failed to initialize Pinterest client.");
-        }
-    }*/
 
     @Override
     public Mono<String> createBoard(String name, String description) {
