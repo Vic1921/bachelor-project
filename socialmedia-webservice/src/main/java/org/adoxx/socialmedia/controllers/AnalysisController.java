e951eea03b9949ca354cfbe1ce9f928fa61eba8c
Victor Binzar <a12022144@unet.univie.ac.at>
2024-07-16 22:00:39 +0300

    Created category endpoint

--- socialmedia-webservice/src/main/java/org/adoxx/socialmedia/controllers/AnalysisController.java
+++ socialmedia-webservice/src/main/java/org/adoxx/socialmedia/controllers/AnalysisController.java
@@ -1,16 +1,13 @@
 package org.adoxx.socialmedia.controllers;
 
 import org.adoxx.socialmedia.models.Comment;
+import org.adoxx.socialmedia.models.ModelFeedbackOverview;
 import org.adoxx.socialmedia.models.SentimentResult;
+import org.adoxx.socialmedia.services.ICommentCategoryService;
 import org.adoxx.socialmedia.services.IPinService;
 import org.adoxx.socialmedia.services.ISentimentAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,10 +21,20 @@ public class AnalysisController {
     @Autowired
     private IPinService pinService;
 
-    @GetMapping("/getAnalysis")
-    public Mono<ResponseEntity<List<SentimentResult>>> getAnalysis(@RequestParam String pinId) {
+    @Autowired
+    private ICommentCategoryService categoryService;
+
+    @GetMapping("/{pinId}}")
+    public List<SentimentResult> getAnalysis(@PathVariable String pinId) {
         List<Comment> comments = pinService.getPinComments(pinId);
 
-        return (Mono<ResponseEntity<List<SentimentResult>>>) analysisService.analyzeComments(comments);
+        return analysisService.analyzeComments(comments);
+    }
+
+    @GetMapping("/categorized/{pinId}")
+    public ModelFeedbackOverview getCategories(@PathVariable String pinId) {
+        List<Comment> comments = pinService.getPinComments(pinId);
+
+        return categoryService.categorizeComments(comments);
     }
 }
