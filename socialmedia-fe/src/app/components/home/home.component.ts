c3ca1165aae0e6a899750eecb02ad29931d6cb17
Victor Binzar <a12022144@unet.univie.ac.at>
2024-08-08 19:42:34 +0300

    Added PrimeNG buttons to the homepage

--- socialmedia-fe/src/app/components/home/home.component.ts
+++ socialmedia-fe/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
