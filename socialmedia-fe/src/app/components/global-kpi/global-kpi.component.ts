e0cc8eb046b39e1d5b368514a30b98340d169c7d
Victor Binzar <a12022144@unet.univie.ac.at>
2024-09-17 23:11:05 +0300

    Polished global kpis

--- socialmedia-fe/src/app/components/global-kpi/global-kpi.component.ts
+++ socialmedia-fe/src/app/components/global-kpi/global-kpi.component.ts
@@ -4,13 +4,15 @@ import { PieChartComponent } from '../pie-chart/pie-chart.component';
 import { CommonModule } from "@angular/common";
 import { ModelFeedbackOverview } from '../../models/model-feedback-overview';
 import {CategoryTableComponent} from "../category-table/category-table.component";
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-global-kpi',
   templateUrl: './global-kpi.component.html',
   styleUrls: ['./global-kpi.component.css'],
   standalone: true,
-  imports: [CommonModule, PieChartComponent, CategoryTableComponent]
+  imports: [CommonModule, PieChartComponent, CategoryTableComponent, CardModule, ButtonModule]
 })
 export class GlobalKPIComponent implements OnInit {
   globalKPI: Map<string, number> = new Map();
