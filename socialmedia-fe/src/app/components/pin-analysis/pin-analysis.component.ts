54a30d6bee16ce7a81b017e68e89ba1e73ec570e
Victor Binzar <a12022144@unet.univie.ac.at>
2024-09-18 12:08:47 +0300

    Disabled Post on Pinterest button after press on Pin Analysis

--- socialmedia-fe/src/app/components/pin-analysis/pin-analysis.component.ts
+++ socialmedia-fe/src/app/components/pin-analysis/pin-analysis.component.ts
@@ -19,6 +19,7 @@ export class PinAnalysisComponent implements OnInit {
   sentimentResults: SentimentResult[] = [];
   sentimentSummary: Map<string, number> = new Map();
   feedbackOverview: ModelFeedbackOverview = { topConcerns: '', favoriteAspects: '', mostRequestedFeatures: '' };
+  isPosting: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -45,8 +46,10 @@ export class PinAnalysisComponent implements OnInit {
   postFeedbackOnPinterest(): void {
     const pinId = this.route.snapshot.paramMap.get('pinId');
     if (pinId) {
+      this.isPosting = true;
       this.dataService.postFeedbackOnPinterest(pinId, this.feedbackOverview).subscribe(() => {
         console.log('Feedback posted on Pinterest');
+        this.isPosting = false;
       });
     }
   }
